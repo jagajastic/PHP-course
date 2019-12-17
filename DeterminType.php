7783b4512978109ef7a35eeeed03a3575f8867e3
jagajastic <josephibrahi@gmail.com>
2019-12-18 00:20:57 +0100

    learn how to check types

---
+++ DeterminType.php
@@ -0,0 +1,22 @@
+<?php
+
+define('NEW_COS', 'Yes I am a constant!');
+
+$intVar = 1234;
+$intVar = '1234';
+$stringVar = 'I\'m a String';
+$stringVar = '1234';
+$boolVar = false;
+$floatVar = 1.234;
+
+// echo is_int($intVar); // return one if true else nothing
+// echo is_int($intVar); // test for string of number
+
+// echo is_string($stringVar); // return 1
+// echo is_string($stringVar); // test for string of number
+
+// echo is_bool($boolVar);
+
+// echo is_float($floatVar);
+
+// echo defined('NEW_COS'); // this check if it is defined or not
