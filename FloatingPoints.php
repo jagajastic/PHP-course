18ed4f1f95b684aeeb4f48e681c2cde058e35401
jagajastic <josephibrahi@gmail.com>
2019-12-18 00:03:18 +0100

    learned floating point number

---
+++ FloatingPoints.php
@@ -0,0 +1,7 @@
+<?php
+
+$float = 1.234; // floating point number
+$scientificNum = 0.1234E4; // scientific positive number
+$scientificNum = 1234E-4; // scientific negative numbers
+
+var_dump($scientificNum);
