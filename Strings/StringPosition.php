3f7df99140f8876e67d2f1d182b7db83db13804c
jagajastic <josephibrahi@gmail.com>
2019-12-18 01:55:01 +0100

    learned string position

---
+++ Strings/StringPosition.php
@@ -0,0 +1,10 @@
+ <?php
+
+$quote = "courage is resistance to fear, mastery of fear, not absent of fear ";
+
+// echo strpos($quote, 'fear');
+// echo strpos($quote, 'fear', 26);
+
+echo strpos($quote, 'z'); // nothing when not found
+
+?>
