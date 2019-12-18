b13d1dc40f74c3b32b054e6412ab044aa7c26b94
jagajastic <josephibrahi@gmail.com>
2019-12-18 01:22:52 +0100

    learned single quotes

---
+++ Strings/SingleQuotes.php
@@ -0,0 +1,8 @@
+<?php
+
+$penny = 'penny';
+$sampleString = 'A $penny saved is a {$penny} earned';
+
+// echo $sampleString;
+$misc = 'St. Patrick\'s Day\n'; // this never at new line rather \n
+echo $misc;
