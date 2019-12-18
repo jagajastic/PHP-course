326b716e907066092c390fa04141a6510dba1c0c
jagajastic <josephibrahi@gmail.com>
2019-12-18 12:00:56 +0100

    learned how to add item to array

---
+++ Arrays/array_push.php
@@ -0,0 +1,8 @@
+<?php
+
+$author = array("Charley Dickens", "Jane Austin", "William Shakespeare");
+
+array_push($author, 'Louis May Alcott'); // add item to array with push
+$author[] = "Martin Athur";
+
+print_r($author);
