d4200766c3d11fa2d731ff1eda06a65b1fb7b2fd
jagajastic <josephibrahi@gmail.com>
2019-12-18 14:55:00 +0100

    learned foreach to iterate over array

---
+++ Arrays/foreach.php
@@ -0,0 +1,11 @@
+<?php
+
+$author = array(
+    "quarky" => "Charles Dickens",
+    "brilliant" => "Jane Austin",
+    "peotic" => "William Shakespaere",
+);
+
+foreach ($author as $key => $val) {
+    echo $key . "=>" . $val . "\n";
+}
