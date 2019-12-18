2ee169593907dd83dbdb7745337b18381979d674
jagajastic <josephibrahi@gmail.com>
2019-12-18 08:53:43 +0100

    learn how to create and initialize array

---
+++ Arrays/IndexedArrays.php
@@ -0,0 +1,14 @@
+<?php
+
+$var = array('James', 'Iyo', 'Jane');
+$arr = ['This', 'is', 'How', 'we', 'roll'];
+$alltypes = ['hello', 1234, 1.23, true];
+$nested = ['hello', 1, ['cali', 'os']];
+
+// print_r($arr);
+// print_r($alltypes);
+print_r($nested);
+
+// print_r($var);
+// print_r($var[5]); //array offset
+// print_r($var);
