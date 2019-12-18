8cce4df169a8861efead6c965bb645161d4adf1f
jagajastic <josephibrahi@gmail.com>
2019-12-18 01:30:02 +0100

    learned double quote

---
+++ Strings/DoubleQuote.php
@@ -0,0 +1,12 @@
+<?php
+
+$penny = "penny";
+$sampleString = "A $penny saved is a {$penny} earned";
+
+// echo $sampleString;
+
+$var = 2;
+// echo $var . "nd place";
+
+$misc = "St. Patrick\"s Day"; // this never at new line rather \n
+echo $misc; // double quote is evaluated
