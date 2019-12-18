5db1b7037d3ed000d2d10e85aa7312fd7e692f9a
jagajastic <josephibrahi@gmail.com>
2019-12-18 08:53:11 +0100

    learned string spliting

---
+++ Strings/StringSplit.php
@@ -0,0 +1,11 @@
+<?php
+
+$quote = "it is a melancholy truth that even great men have their poor relations.";
+
+// $varArray = str_split($quote); // split each charact
+// $varArray = str_split($quote, 8); // split each charact by length of 8
+$varArray = str_split($quote, 2); // split each charact by length of 8
+
+// print_r($varArray);
+
+print_r($varArray);
