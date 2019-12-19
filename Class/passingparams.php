89baa200b2b1476ed70931f05525ddb1d3091309
jagajastic <josephibrahi@gmail.com>
2019-12-19 11:13:53 +0100

    learned how to pass params to constructor

---
+++ Class/passingparams.php
@@ -0,0 +1,30 @@
+<?php
+
+class Person
+{
+    const AVG_LIFE_SPAN = 79;
+    public $firstName;
+    public $lastName;
+    public $yearBorn;
+
+    public function __construct($tempFirst, $tempLast, $tempBorn)
+    {
+        $this->firstName = $tempFirst;
+        $this->lastName = $tempLast;
+        $this->yearBorn = $tempBorn;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName($tempName)
+    {
+        $this->firstName = $tempName;
+    }
+}
+
+$myObject = new Person("Samuel Langhorne", "Clement", 1899);
+
+echo $myObject->getFirstName();
