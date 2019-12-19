835beae3fc8ab9a3db4354e2e356d52e21eb4d15
jagajastic <josephibrahi@gmail.com>
2019-12-19 06:34:50 +0100

    I learned multidimensional array

---
+++ Arrays/multidimensionalArray.php
@@ -0,0 +1,18 @@
+<?php
+
+$author = array(
+    "Male" => array(
+        "Charles Dickens" => array("A christmas Carol", "Oliver Twist"),
+        "William Shakespaere" => array("Romeo & Juliet", "Richard III"),
+        "Mark Twain" => array("The Adventures of Tom sawyer", "The Prince and the Pauper"),
+    ),
+    "Female" => array(
+        "L. M. Montgomery" => array("Anne of Green  Gable", "Anne of Avonlee"),
+        "Louisa May Alcott" => array("Little Women"),
+    ),
+
+);
+
+print_r($author)
+
+?>
