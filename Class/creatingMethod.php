290d3a118e541a16083dfb0b530e33cdcd536e99
jagajastic <josephibrahi@gmail.com>
2019-12-19 10:49:48 +0100

    learned how to add method to class

---
+++ Class/creatingMethod.php
@@ -0,0 +1,21 @@
+<?php
+
+class Person
+{
+    const AVG_LIFE_SPAN = 79;
+    public $firstName = "Samuel Langhorne";
+    public $lastName = "Clement";
+    public $yearBorn = 1899;
+
+    public function getFirstName()
+    {
+
+    }
+
+    public function setFirstName()
+    {
+
+    }
+}
+
+$myObject = new Person();
