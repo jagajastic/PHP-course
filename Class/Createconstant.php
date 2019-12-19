04132ffe9fc37efa08f93d6453b5ef105d58d3e5
jagajastic <josephibrahi@gmail.com>
2019-12-19 10:13:37 +0100

    I learned how to define a constant in class

---
+++ Class/Createconstant.php
@@ -0,0 +1,11 @@
+<?php
+
+class Person
+{
+    const AVG_LIFE_SPAN = 79;
+    public $firstName = "Samuel Langhorne";
+    public $lastName = "Clement";
+    public $yearBorn = 1899;
+}
+
+$myObject = new Person();
